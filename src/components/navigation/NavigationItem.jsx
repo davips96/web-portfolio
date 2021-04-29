c359e1cd7aafc9f393ca0629184fa95167753beb
davips96 <davips96@gmail.com>
2021-04-28 20:24:10 -0400

    :bomb: Reset project

--- src/components/navigation/NavigationItem.jsx
+++
@@ -1,7 +0,0 @@
-export default function NavigationItem({ children, ...props }) {
-  return (
-    <div className="ml-8 text-grey-100" key={props.key}>
-      {children}
-    </div>
-  );
-}
--- src/components/navigation/NavigationTitle.jsx
+++
@@ -1,3 +0,0 @@
-export default function NavigationTitle({ children }) {
-  return <div className="text-3xl text-grey-100">{children}</div>;
-}
