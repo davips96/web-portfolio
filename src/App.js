5a1eef5d8407f12bd3dc7625ef7f01738ea7900f
davips96 <davips96@gmail.com>
2021-04-28 20:24:10 -0400

    :bomb: Reset project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import NavigationBar from "./components/navigation/NavigationBar";
-import sections from "./sections";
-
 function App() {
-  return (
-    <div className="flex">
-      <NavigationBar />
-      <div className="flex flex-col w-screen">
-        {sections.map((section) => (
-          <section.component />
-        ))}
-      </div>
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
