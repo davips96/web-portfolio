388d6bd140f43c5fc3857555864803948f55e26c
davips96 <davips96@gmail.com>
2022-03-12 21:14:57 -0500

    Complete update and refactor

--- src/components/navigation/NavigationItem.jsx
+++
@@ -1,3 +0,0 @@
-export default function NavigationItem({ children }) {
-  return <li>{children}</li>;
-}
--- src/components/navigation/NavigationTitle.jsx
+++
@@ -1,3 +0,0 @@
-export default function NavigationTitle({ children }) {
-  return <li className="text-2xl font-bold">{children}</li>;
-}
